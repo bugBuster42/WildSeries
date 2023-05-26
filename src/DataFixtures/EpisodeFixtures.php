a0a9342e0769d346bba4dd9da0c4117e0264d7a1
thomaslavalade <thomaslavalade@gmail.com>
2023-05-26 16:16:10 +0200

    quest12_finish

--- src/DataFixtures/EpisodeFixtures.php
+++ src/DataFixtures/EpisodeFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
 use App\Entity\Episode;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -11,53 +12,15 @@ class EpisodeFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        $episodes = [
-            [
-                'title' => 'Descenso',
-                'number' => '1',
-                'synopsis' => 'Chilean drug chemist Cockroach brings his product to Colombian smuggler Pablo Escobar. DEA agent Steve Murphy joins the war on drugs in Bogota.',
-                'season' => '1',
-            ],
-            [
-                'title' => 'The Sword of Simón Bolivar',
-                'number' => '2',
-                'synopsis' => 'Communist radical group M-19 makes a move against the narcos, while Murphy gets an education in Colombian law enforcement from his new partner Peña.',
-                'season' => '1',
-            ],
-            [
-                'title' => 'The Men of Always',
-                'number' => '3',
-                'synopsis' => 'Murphy encounters the depths of government corruption when he and Peña try to derail Escobar\'s political ambitions by proving he\'s a narco.',
-                'season' => '1',
-            ],
-            [
-                'title' => 'Free at Last',
-                'number' => '1',
-                'synopsis' => 'In the aftermath of a massive military effort to take Pablo into custody, the family reunites while enemies worry. Steve and Connie fight about safety.',
-                'season' => '2',
-            ],
-            [
-                'title' => 'Cambalache',
-                'number' => '2',
-                'synopsis' => 'Tata gets impatient with life on the run. Pablo responds to President Gaviria\'s reward offer. Steve and Javier meet their new boss.',
-                'season' => '2',
-            ],
-            [
-                'title' => 'Our Man in Madrid',
-                'number' => '3',
-                'synopsis' => 'President Gaviria has a new job for an old colleague. The Search Bloc\'s new tactics shake up Pablo, but also unsettle Steve and Javier.',
-                'season' => '2',
-            ],
-
-        ];
+        $faker = Factory::create();
 
 
-        foreach ($episodes as $episode) {
+        for ($i = 0; $i < 550; $i++) {
             $newEpisode = new Episode();
-            $newEpisode->setNumber($episode['number']);
-            $newEpisode->setTitle($episode['title']);
-            $newEpisode->setSynopsis($episode['synopsis']);
-            $newEpisode->setSeason($this->getReference('season_' . $episode['season']));
+            $newEpisode->setNumber(($i % 10) + 1);
+            $newEpisode->setTitle($faker->sentence(3));
+            $newEpisode->setSynopsis($faker->paragraphs(3, true));
+            $newEpisode->setSeason($this->getReference('season_' . $i % 55));
             $manager->persist($newEpisode);
         }
 
--- src/DataFixtures/SeasonFixtures.php
+++ src/DataFixtures/SeasonFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
 use App\Entity\Season;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -11,31 +12,18 @@ class SeasonFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        $seasons = [
-            [
-                'number' => '1',
-                'year' => '2O15',
-                'description' => 'Dans les années 1980, Pablo Escobar, un petit contrebandier colombien, décide de se lancer dans la production et l\'exportation de cocaïne dont la rentabilité va lui permettre de fonder le puissant Cartel de Medellin.',
-                'program' => 'program_Narcos',
-            ],
-            [
-                'number' => '2',
-                'year' => '2O16',
-                'description' => 'Après son évasion de la prison, la Cathédrale, Pablo Escobar et sa famille deviennent des fugitifs permanents. Le narco trafiquant continue pourtant ses affaires. Les forces de police, toujours avec Javier Peña et Steve Murphy, le traquent sans relâche. Mais de nouveaux éléments entrent en jeu.',
-                'program' => 'program_Narcos',
-            ],
-        ];
+        $faker = Factory::create();
 
 
+        for ($i = 0; $i < 55; $i++) {
+            $season = new Season();
+            $season->setNumber(($i % 5) + 1);
+            $season->setYear($faker->year());
+            $season->setDescription($faker->paragraphs(3, true));
+            $season->setProgram($this->getReference('program_' . $i % 11));
+            $manager->persist($season);
 
-        foreach ($seasons as $season) {
-            $newSeason = new Season();
-            $newSeason->setNumber($season['number']);
-            $newSeason->setYear($season['year']);
-            $newSeason->setDescription($season['description']);
-            $newSeason->setProgram($this->getReference($season['program']));
-            $manager->persist($newSeason);
-            $this->addReference('season_' . $season['number'], $newSeason);
+            $this->addReference('season_' . $i, $season);
         }
 
         $manager->flush();
