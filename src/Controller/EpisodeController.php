9128f1d367010cc266d2b7c113d52416bdb1bcae
thomaslavalade <thomaslavalade@gmail.com>
2023-07-04 11:51:55 +0200

    finish quest21

--- src/Controller/EpisodeController.php
+++ src/Controller/EpisodeController.php
@@ -2,15 +2,17 @@
 
 namespace App\Controller;
 
-use App\Entity\Episode;
 use App\Entity\Season;
+use App\Entity\Episode;
 use App\Form\EpisodeType;
+use Symfony\Component\Mime\Email;
 use App\Repository\EpisodeRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\MailerInterface;
 
 #[Route('/episode')]
 class EpisodeController extends AbstractController
@@ -24,7 +26,7 @@ class EpisodeController extends AbstractController
     }
 
     #[Route('/new', name: 'app_episode_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EpisodeRepository $episodeRepository, SluggerInterface $slugger): Response
+    public function new(Request $request, EpisodeRepository $episodeRepository, SluggerInterface $slugger, MailerInterface $mailer): Response
     {
         $episode = new Episode();
         $form = $this->createForm(EpisodeType::class, $episode);
@@ -34,6 +36,13 @@ class EpisodeController extends AbstractController
             $slug = $slugger->slug($episode->getTitle());
             $episode->setSlug($slug);
             $episodeRepository->save($episode, true);
+            $email = (new Email())
+                ->from($this->getParameter('mailer_from'))
+                ->to('thomaslavalade@gmail.com')
+                ->subject('Un nouvel épisode est dispo !')
+                ->html($this->renderView('Episode/newEpisodeEmail.html.twig', ['episode' => $episode]));
+            $mailer->send($email);
+
             $this->addFlash('success', 'Le nouvel épisode a bien été ajouté');
             return $this->redirectToRoute('app_episode_index', [], Response::HTTP_SEE_OTHER);
         }
